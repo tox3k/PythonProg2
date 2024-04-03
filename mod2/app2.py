06fb7bd5ff26c3da11295327f671c9980bec737f
Андрей Колчин <Andrei.Kolchin@urfu.me>
2024-04-04 03:48:20 +0500

    mod2 Done!

--- mod2/app2.py
+++ mod2/app2.py
@@ -116,6 +116,52 @@ def file_preview(SIZE, RELATIVE_PATH):
     preview = open(abs_path, 'r').read(SIZE)
     return f'<abs_path>{abs_path}</abs_path> <result_size>{len(preview)}</result_size><br><result_text>{preview}</result_text>'
 
-    pass
+
+storage = {}
+@app.route('/add/<date>/<int:number>', methods=['POST', 'GET'])
+def save_date(date, number):
+    date = datetime.fromisoformat(date)
+    storage.setdefault(date.year, {}).setdefault(date.month, {}).setdefault(date.day, {})
+    storage[date.year][date.month][date.day] = number
+    return ''
+
+@app.route('/calculate/<int:year>')
+def calculate_year(year):
+    if year not in storage:
+        storage[year] = {}
+    sum = 0
+    for k1, v1 in storage[year].items():
+        for k2, v2 in v1.items():
+            sum += v2
+
+    return f'Сумма за {year} год: {sum} руб.'
+
+months = {
+    1:'январь',
+    2:'февраль',
+    3: 'март',
+    4: 'апрель',
+    5: 'май',
+    6: 'июнь',
+    7: 'июль',
+    8: 'август',
+    9: 'сентябрь',
+    10: 'октябрь',
+    11: 'ноябрь',
+    12: 'декабрь',
+
+}
+@app.route('/calculate/<int:year>/<int:month>')
+def calculate_year_month(year, month):
+    if year not in storage:
+        storage[year] = {}
+    if month not in storage[year]:
+        storage[year][month] = {}
+    sum = 0
+    for k, v in storage[year][month].items():
+        sum += v
+
+    return f'Сумма за {months[month]} месяц {year} года: {sum} руб.'
+
 if __name__ == '__main__':
     app.run(debug=True)
--- mod2/static/script.js
+++ mod2/static/script.js
@@ -21,3 +21,38 @@ function toggleButton(){
         bt.setAttribute("disabled", "disabled")
     }
 }
+
+function saveOperation(){
+    var date = document.getElementById("date").value;
+    var money = document.getElementById("money").value;
+    
+    fetch('/add/'+date.toString()+'/'+money.toString());
+}
+
+function toggleButtonOperation(){
+    var money = document.getElementById("money").value;
+    var date = document.getElementById("date").value
+    var bt = document.getElementById("submit_operation")
+
+    if (money && date){
+        bt.removeAttribute("disabled", "")
+    }
+
+    else{
+        bt.setAttribute("disabled", "disabled")
+    }
+}
+
+function calculateYear(){
+    var year = document.getElementById("year_to_calculate").value.toString().slice(0, 4)
+    window.open('/calculate/'+year, '_blank')
+}
+
+function calculateYearMonth(){
+    element = document.getElementById("year_to_calculate").value.toString()
+    var year = element.slice(0, 4)
+    var month = element.slice(5, 7)
+    if(month[0] == '0')
+        month = month[1]
+    window.open('/calculate/'+year+'/'+month, '_blank')
+}
